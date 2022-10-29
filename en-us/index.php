95ebab7ec5f288cff588385e5a6c5763d96b6446
znc15 <jimmybandass21@gmail.com>
2022-10-29 15:00:07 +0800

    Update English Html

---
+++ en-us/index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="“zh-CN”">
+  <!--   Header Start   -->
+  <head>
+    <!--   Html info Start   -->
+    <meta name="author" content="" />
+    <meta charset="utf-8" />
+    <meta name="theme-color" content="#ffffff" />
+    <meta name="description" content="TCB Work API" />
+    <meta name="keywords" content="TCB Work API" />
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TCB Work API</title>
+    <!--   Html info End   -->
+
+    <!--   CSS Start   -->
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/assets/css/flaticon-set.css" rel="stylesheet" />
+    <link href="/assets/css/magnific-popup.css" rel="stylesheet" />
+    <link href="/assets/css/owl.carousel.min.css" rel="stylesheet" />
+    <link href="/assets/css/owl.theme.default.min.css" rel="stylesheet" />
+    <link href="/assets/css/animate.css" rel="stylesheet" />
+    <link href="/assets/css/bootsnav.css" rel="stylesheet" />
+    <link href="/assets/css/style.css" rel="stylesheet" />
+    <link href="/assets/css/responsive.css" rel="stylesheet" />
+    <link href="/assets/css/mdui.min.css" rel="stylesheet" />
+    <link href="/assets/css/mdui.css" rel="stylesheet" />
+    <link href="/assets/css/index.e8409b3c.css" rel="stylesheet" />
+    <link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/css/all.css" rel="stylesheet">
+    <link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/css/fontawesome.css" rel="stylesheet">
+    <link href="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/css/fontawesome.min.css" rel="stylesheet">
+    <!--   CSS End   -->
+  </head>
+  <!--   Header End   -->
+  <!-- Body Start -->
+  <body class="mdui-drawer-body-left mdui-drawer-full-height" style="padding-top: 100px;">
+    <!-- APPBAR Start -->
+    <header>
+    <div class="mdui-appbar mc-appbar mdui-appbar-fixed mdui-shadow-0"> 
+    <div class="mdui-toolbar toolbar mdui-text-color-black mdui-shadow-3"> 
+    <button id="toggle" class="drawer mdui-btn mdui-btn-icon mdui-ripple">
+      <i class="mdui-icon material-icons">menu</i>
+    </button> 
+    <a href="javascript:;" class="mdui-typo-title">TCB Work</a> 
+    <div class="mdui-toolbar-spacer"></div> 
+    <a onclick="javascript:location.reload();" class="mdui-btn mdui-btn-icon">
+       <i class="mdui-icon material-icons">refresh</i>
+    </a> 
+    <div class="mdui-contain"> 
+     <button class="drawer mdui-btn mdui-btn-icon mdui-ripple" mdui-menu="{target: '#example-2'}">
+      <i class="mdui-icon material-icons">language</i>
+     </button> 
+     <ul class="mdui-menu" id="example-2"> 
+      <li class="mdui-menu-item"> <a href="/zh-cn" class="mdui-ripple">
+        <i class="mdui-menu-item-icon mdui-icon material-icons">remove_red_eye</i>Chinese/中文</a></li> 
+      <li class="mdui-menu-item"> <a href="/en-US" class="mdui-ripple">
+        <i class="mdui-menu-item-icon mdui-icon material-icons">remove_red_eye</i>English/英文</a></li> 
+     </ul> 
+    </div> 
+    </div> 
+    </div>
+    </header>
+    <!-- APPBAR End -->
+
+    <!-- List Start -->
+    <div class="mdui-drawer mdui-shadow-1 mc-drawer" id="drawer">
+      <div class="mdui-list">
+      <a href="/zh-cn" class="mdui-list-item mdui-ripple mdui-list-item-active">
+        <i class="mdui-list-item-icon mdui-icon material-icons">home</i>
+        <div class="mdui-list-item-content">Home</div>
+      </a>
+      <li class="mdui-subheader">API 链接/API Link</li>
+      <a href="/zh-cn/lz/" class="mdui-list-item mdui-ripple">
+        <i class="mdui-list-item-icon mdui-icon material-icons">free_breakfast</i>
+        <div class="mdui-list-item-content">LanZou Cloud url</div>
+      </a>
+      <a href="/zh-cn/imageapi" class="mdui-list-item mdui-ripple">
+        <li class="mdui-list-item-icon mdui-icon material-icons">cloud_download</li>
+        <div class="mdui-list-item-content">Random Image API</div>
+      </a>
+      <li class="mdui-subheader">联系方式/Contact Details</li>
+      <a href="https://jq.qq.com/?_wv=1027&k=hFdySg2N" class="mdui-list-item mdui-ripple">
+        <i class="mdui-list-item-icon mdui-icon material-icons">email</i>
+        <div href="https://jq.qq.com/?_wv=1027&k=hFdySg2N" class="mdui-list-item-content">QQ Group</div>
+      </a>
+      <li class="mdui-subheader">友情链接/Link</li>
+      <a href="https://www.miaofile.com" class="mdui-list-item mdui-ripple">
+        <i class="fa-brands fa-internet-explorer mdui-list-item-icon"></i>
+        <div class="mdui-list-item-content">Miao File</div>
+      </a>
+     </div>
+     <div class="copyright">
+      <p>
+        <br>© 2022
+        <br>TCB Work's
+        <br>Minecraft Server.
+        <br>Design with by TCB Work's HTML.
+        <br>Powered by
+        <a href="https://mdui.org" target="_blank">MDUI&Ali-a.cn</a>
+      </p>
+    </div>
+  </div>
+  <!-- List End -->
+
+  <!-- Home#1 Start -->
+  <div id="page-index" class="mdui-container">
+    <div class="banner">
+      <h1>
+        <strong>T</strong>CB <strong>W</strong>ork's
+        <strong>A</strong>PI
+      </h1>
+      <div class="meta">
+      A collection of all API links and usage methods under TCB Work.
+      </div>
+      <div class="actions">
+        <a href="/docs" class="mdui-btn mdui-ripple" target="_blank"
+          >Join Our QQ Group</a
+        ><a
+          href="/khd/"
+          class="mdui-btn mdui-ripple"
+          >Browse now</a
+        >
+      </div>
+      <div class="more-meta mdui-typo">
+        <span class="current">Later Version：v1.0.0</span
+        ><span
+          ><a
+            href="https://github.com/MiaoFile"
+            rel="nofollow"
+            target="_blank"
+            >Github</a
+          ></span
+        ><span
+          ><a
+            href="https://gitee.com/roundcloud_0"
+            rel="nofollow"
+            target="_blank"
+            >Gitea</a
+          ></span
+        ><span
+          ><a
+            href="https://jq.qq.com/?_wv=1027&k=hFdySg2N"
+            rel="nofollow"
+            target="_blank"
+            >QQ Group：913250083</a
+          ></span
+        >
+      </div>
+    </div>
+  </div>
+  <!-- Home#1 End -->
+
+  <!-- Thanks Start -->
+  <article id="aboutus" name="aboutus">
+    <div class="utf-team-area carousel-shadow default-thanks-padding">
+        <div class="container">
+            <div class="row">
+                <div class="utf-site-heading text-center">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h2><span>Special </span>Thanks to</h2>
+                        <p>Special thanks to the server and website template provided by 熊可狸</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <center>
+                <div class="col-md-12">
+                    <div class="utf-team-items utf-team-carousel owl-carousel owl-theme">
+                        <!--Start KumaAli-->
+                        <div class="item">
+                            <a target="_blank" href="http://ali-a.cn" title="Blog">
+                                <div class="thumb mdui-ripple">
+                                    <img src="https://m1.miaomc.cn/uploads/20210117_d75593a0c9af2.png" />
+                                </div>
+                            </a>
+                            <div class="info mdui-ripple">
+                                <h4><a target="_blank" href="http://ali-a.cn" title="Blog">熊可狸</a></h4>
+                                <span>Website source code provider, Minecraft Java version server underlying architecture developer</span>
+                                <p>Hmm... that's it, it's a very homely dead house! I usually like to toss! There is so much more to learn</p>
+                            </div>
+                        </div>
+                        <!--Start KumaAli-->
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </center>
+  </article>
+  <!-- Thanks End -->
+  
+  <!-- JavaScript Start -->
+  <script src="/assets/js/mdui.min.js"></script>
+  <script src="/assets/js/mdui.js"></script>
+  <script src="/assets/js/jquery-3.2.1.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
+  <script src="/assets/js/equal-height.min.js"></script>
+  <script src="/assets/js/jquery.appear.js"></script>
+  <script src="/assets/js/jquery.easing.min.js"></script>
+  <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="/assets/js/modernizr.custom.js"></script>
+  <script src="/assets/js/owl.carousel.min.js"></script>
+  <script src="/assets/js/wow.min.js"></script>
+  <script src="/assets/js/isotope.pkgd.min.js"></script>
+  <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
+  <script src="/assets/js/count-to.js"></script>
+  <script src="/assets/js/bootsnav.js"></script>
+  <script src="/assets/js/typed.js"></script>
+  <script src="/assets/js/YTPlayer.min.js"></script>
+  <script src="/assets/js/custom_script.js"></script>
+  <script src="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/js/all.js"></script>
+  <script src="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/js/all.min.js"></script>
+  <script src="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/js/fontawesome.js"></script>
+  <script src="https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/js/fontawesome.min.js"></script>
+  <script src="https://eqcn.ajz.miesnfu.com/wp-content/plugins/wp-3d-pony/live2dw/lib/L2Dwidget.min.js"></script>
+  <!-- JavaScript End -->
+
+  </body>
+  <!-- Body End -->
+
+  <!-- Footer Start -->
+  <footer>
+    <!-- Live2d Start -->
+    <script>
+      L2Dwidget.init({
+          "model": {
+              jsonPath: "/assets/model/xiaomai/xiaomai.model.json",
+              "scale": 1
+          },
+          "display": {
+              "position": "right", //显示位置左、右
+              "width": 75,  // 宽度
+              "height": 150, //高度
+          },
+          "mobile": {
+              "show": false,	// 移动端显示
+              "scale": 0.2	//移动端显示缩小比例
+          },
+          "react": {
+              "opacityDefault": 0.9,	//默认透明度
+              "opacityOnHover": 0.2	//鼠标选中透明度
+          }
+      });
+    </script>
+    <!-- Live2d Start -->
+  </footer>
+  <!-- Footer end -->
+
+</html>
